3facce15a9c82da11633311cb1a6a5679a5c5e27
kimjongsoo97 <rlwhdtn97@naver.com>
2024-11-06 22:10:42 +0900

    프로젝트 시작

---
+++ src/test/java/com/example/kb_backend/KbBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kb_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
