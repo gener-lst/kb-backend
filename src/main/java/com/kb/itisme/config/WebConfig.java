e496776265b9087259f96aa7c4e224b2e02570d1
rlatneorp@gmail.com <rlatneorp@gmail.com>
2024-11-12 14:06:07 +0900

    feat : 초기설정 및 트렌드뉴스 가져오기

---
+++ src/main/java/com/kb/itisme/config/WebConfig.java
@@ -0,0 +1,16 @@
+package com.kb.itisme.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5174") // 프론트엔드 포트를 지정
+                .allowedMethods("GET", "POST");
+    }
+}
